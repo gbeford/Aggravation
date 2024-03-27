ba79cfd97992daf8c42698f12e5fd35ff9c5aedf
Bierenga <dominicbierenga@rocketcentral.com>
2024-03-27 14:49:34 -0400

    add colors and toggling to game-card, format dashboard

--- src/app/game-card/game-card.component.ts
+++ src/app/game-card/game-card.component.ts
@@ -12,7 +12,26 @@ import { IRound } from '../services/round';
 })
 export class GameCardComponent {
   rounds: IRound[];
+  color: string = '';
+  colorIndex = 0;
   constructor(service: GameService) { 
-    this.rounds = service.getRounds(); 
+    this.rounds = service.getRounds();
+  }
+
+  getColor(): string {
+    const colorsArray = ['#fe6100', '#785ef0', '#dc267f'];
+    this.color = colorsArray[this.colorIndex];
+    this.colorIndex++;
+    if (this.colorIndex > 2) this.colorIndex = 0;
+    return this.color;
+  }
+
+  toggleOpacity(roundNumber: number): void {
+    const list = document.querySelectorAll('div.round-container');
+    list.forEach((element) => {
+      element.classList.remove('opaque');
+    });
+    const selectedElement = document.getElementById(`round-container-${roundNumber}`);
+    selectedElement?.classList.add('opaque');
   }
 }
