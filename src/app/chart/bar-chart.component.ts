f4d87177877e63a88481509b5ba3d225be1a3e63
Beford, Gina <ginabeford@rocketmortgage.com>
2024-04-02 09:32:07 -0400

    style chart

--- src/app/chart/bar-chart.component.ts
+++ src/app/chart/bar-chart.component.ts
@@ -16,38 +16,38 @@ export class BarChartComponent implements OnInit {
   @ViewChild(BaseChartDirective) chart: BaseChartDirective<'bar'> | undefined;
 
   chartLabels: string[] = [];
-  // public chartType = 'bar' as const;
-  // chartOptions = {
-  //   responsive: true
-  // };
+  scoreData = {};
+  public barChartType = 'bar' as const;
+  barChartOptions = { responsive: true };
+  public barChartData: ChartData<'bar'> = { datasets: [] };
 
   constructor(private service: SignalGameService) {
 
-    // this.scoreData = service.playerData().map(r => r.score);
+
     // this.barChartData = [
     //   { data: this.scoreData, label: 'Series A' }
     // ];
   }
   ngOnInit() {
     this.chartLabels = this.service.playerData().map(r => r.name);
-    console.log('lable', this.chartLabels)
+    console.log('label', this.chartLabels)
+    this.scoreData = this.service.playerData().map(r => r.score);
+    this.createChart();
   }
 
-  barChartOptions = {
-    responsive: true
-  };
 
-  public barChartType = 'bar' as const;
 
-  public barChartData: ChartData<'bar'> = {
-    labels: ['2006', '2007', '2008', '2009', '2010', '2011', '2012'],
-    // labels: this.chartLabels,
-    datasets: [
-      { data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A' },
-      // { data: [28, 48, 40, 19, 86, 27, 90], label: 'Series B' },
-    ],
-  };
-
+  createChart() {
+    this.barChartData = {
+      //labels: ['2006', '2007', '2008', '2009', '2010', '2011', '2012'],
+      labels: this.chartLabels,
+      datasets: [
+        // { data: (this.scoreData), label: 'Series A' },
+        { data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A' },
+        // { data: [28, 48, 40, 19, 86, 27, 90], label: 'Series B' },
+      ],
+    };
+  }
   // events
   public chartClicked({
     event,
