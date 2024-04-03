c193192b3fbdb9e6800ae9315735748360cef6aa
Beford, Gina <ginabeford@rocketmortgage.com>
2024-04-03 15:26:26 -0400

    add console log

--- src/app/chart/bar-chart.component.ts
+++ src/app/chart/bar-chart.component.ts
@@ -32,6 +32,7 @@ export class BarChartComponent implements OnInit {
     this.chartLabels = this.service.playerData().map(r => r.name);
     console.log('label', this.chartLabels)
     this.scoreData = this.service.playerData().map(r => r.score);
+    console.log('score', this.scoreData);
     this.createChart();
   }
 
@@ -42,7 +43,7 @@ export class BarChartComponent implements OnInit {
       //labels: ['2006', '2007', '2008', '2009', '2010', '2011', '2012'],
       labels: this.chartLabels,
       datasets: [
-        // { data: (this.scoreData), label: 'Series A' },
+      //  { data: (this.scoreData), label: 'Score' },
         { data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A' },
         // { data: [28, 48, 40, 19, 86, 27, 90], label: 'Series B' },
       ],
