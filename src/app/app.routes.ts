87b77af6fcfc3b7c74a4411bf51b0d4704a2b049
Gina Beford <gbeford@gmail.com>
2025-01-10 15:18:38 -0500

    remove test

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,5 @@
 import { Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
-import { TestComponent } from './test/test.component';
 import { GameCardComponent } from './game-card/game-card.component';
 import { EntryScoreComponent } from './entry-score/entry-score.component';
 import { SignalDashboardComponent } from './dashboard/signal-dashboard/signal-dashboard.component';
